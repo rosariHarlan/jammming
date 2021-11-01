c82895a88733fe1e387dcdaae895dea559a9797b
“Rosari <rosari.harlan@gmail.com>
2021-11-01 11:34:53 +0100

    First commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import "./SearchBar.scss";
+
+const SearchBar = () => {
+  return (
+    <div className="SearchBar">
+      <input placeholder="Enter A Song, Album, or Artist" />
+      <button className="SearchButton">SEARCH</button>
+    </div>
+  );
+};
+
+export default SearchBar;
